e4fb69ab94c0211662f700671bf407a5e1174ac9
Laser Nite <lasernite@gmail.com>
2015-02-02 15:51:30 -0500

    break long words as to not break page, model only accepts text length 4..21012, still need to add validation response to view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
 	belongs_to :tag
 	has_many :ptags
+
+	validates_length_of :text,
+  		:within => 4..21012,
+  		:too_short => 'Post too short, 4 character minimum',
+  		:too_long => 'Post too long, 21012 character maximum'
 end
