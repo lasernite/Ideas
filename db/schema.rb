ffe88540363e427c20076b077ac5d1afdd2dfe21
Laser Nite <lasernite@gmail.com>
2015-02-09 22:16:46 -0500

    database working locally again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203041851) do
+ActiveRecord::Schema.define(version: 20150208033647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,14 +26,14 @@ ActiveRecord::Schema.define(version: 20150203041851) do
     t.text     "text"
     t.integer  "post_id"
     t.integer  "parent_id"
-    t.integer  "ip"
+    t.integer  "ip",         limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
     t.text     "text"
-    t.integer  "ip"
+    t.integer  "ip",         limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
