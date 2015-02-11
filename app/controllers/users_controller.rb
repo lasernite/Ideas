9bb9b04174c65fd5657621c65147b9e7fba607d1
Laser Nite <lasernite@gmail.com>
2015-02-11 01:08:04 -0500

    notifications complete with ability to view all paginated, else 12 in track, still need to mobile optimize. Added home button to other pages that were missing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+  	# Get user by IP
   	@user = User.find_by(ip:request.remote_ip.split('.').join())
-  	@notifications = Notification.where("? = ANY (uip)", @user.ip).reverse
+  	# Get most recent notifications (12) and all notifications associated with IP
+  	@notifications = Notification.where("? = ANY (uip)", @user.ip).reverse[0..11]
+  	@all_notifications = Notification.where("? = ANY (uip)", @user.ip).reverse.paginate(:page => params[:page], :per_page => 12)
   end
 end
