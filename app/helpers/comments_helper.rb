a1978a9a80b3a1a056d364a3efa1bdd100527f26
Laser Nite <lasernite@gmail.com>
2015-02-12 04:43:11 -0500

    added #tags and /t/tags to comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,9 +3,18 @@ module CommentsHelper
 	 	comments.each do |comment| 
 	   		comment_spliced = comment.text.split 
 	   		comment_spliced.map! do |piece| 
+				# Map in tag link for #tag
+	  			if piece.starts_with?('#')
+          			unless piece == nil
+  	  					link_to(piece.downcase, atag_path(Atag.find_by(tag:piece[1..-1].downcase)),
+  	  					:class => 'post_hashtag', :remote => true, :onclick => "location.href='#top'")
+          			end
 		   		# Map in URL link 
-	     		if piece.starts_with?('www.','http://','https://') 
+	     		elsif piece.starts_with?('www.','http://','https://') 
 	       			link_to(piece, piece, target: '_blank') 
+	       		# Map in /t/community links
+        		elsif piece.downcase.starts_with?('/t/')
+          			link_to(piece, piece)
 		   		# Map regular string piece back to string 
 	     		else 
 	       			piece 
