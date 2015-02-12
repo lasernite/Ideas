e4ba433add6b3e6fa86b38d18fb6a17d157b3f7f
Laser Nite <lasernite@gmail.com>
2015-02-12 04:28:24 -0500

    moved splice comments to helper method

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,17 @@
 module CommentsHelper
+	def splice_comments(comments, spliced) 
+	 	comments.each do |comment| 
+	   		comment_spliced = comment.text.split 
+	   		comment_spliced.map! do |piece| 
+		   		# Map in URL link 
+	     		if piece.starts_with?('www.','http://','https://') 
+	       			link_to(piece, piece, target: '_blank') 
+		   		# Map regular string piece back to string 
+	     		else 
+	       			piece 
+	     		end 
+	   		end 
+	   		spliced.append(comment_spliced) 
+	 	end 
+	end
 end
